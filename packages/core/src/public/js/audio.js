111fcfd6babbcd8b51ae56edd5369c5a01a8aa38
Antony Jones <aj@desirableobjects.co.uk>
2017-02-13 18:13:44 +0000

    Insert karma

--- packages/core/src/public/js/audio.js
+++ packages/core/src/public/js/audio.js
@@ -1,13 +1,28 @@
 'use strict'
 
-window.VUDASH = window.VUDASH || {};
-var VUDASH = window.VUDASH;
+window.VUDASH = window.VUDASH || {}
+var VUDASH = window.VUDASH
 
-var Player = function() {};
-
-Player.prototype.play = function(data) {
-  var snd = new Audio(data);
-  snd.play();
+var Player = function () {
+  this.isPlaying = false
 }
 
-VUDASH.player = new Player();
+Player.prototype.isPlaying = function () {
+  return this.isPlaying
+}
+
+Player.prototype.play = function (data) {
+  const self = this
+  var snd = new Audio(data)
+  snd.addEventListener('playing', function () {
+    console.log('PLAYING AUDIO')
+    self.isPlaying = true
+  }, true)
+  snd.addEventListener('ended', function () {
+        console.log('STOPPED PLAYING AUDIO')
+    self.isPlaying = false
+  }, true)
+  snd.play()
+}
+
+VUDASH.player = new Player()
